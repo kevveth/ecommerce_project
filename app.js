835393ff5628d6e31ecba3601cd5743da0c7f9a7
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-10-06 21:21:48 -0700

    refactor: move GET request handlers to separate route files

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const db = require('./db'); // Import the database module
-const usersRouter = require('./routes/users');
+
+const usersRouter = require('./routes/users'),
+        productsRouter = require('./routes/products'),
+        ordersRouter = require('./routes/orders'),
+        orderItemsRouter = require('./routes/order_items'),
+        cartsRouter = require('./routes/carts'),
+        cartItemsRouter = require('./routes/cart_items');
 
 const app = express();
 const port = process.env.port || 3000; 
@@ -10,56 +16,11 @@ app.get('/',(request,response)=>{
    });
 
 app.use('/users', usersRouter);
-
-app.get('/products', async (req, res) => {
-    try {
-      const result = await db.query('SELECT * FROM products');
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error fetching products');
-    }
-  });
-  
-  app.get('/carts', async (req, res) => {
-    try {
-      const result = await db.query('SELECT * FROM carts');
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error fetching carts');
-    }
-  });
-  
-  app.get('/cart_items', async (req, res) => {
-    try {
-      const result = await db.query('SELECT * FROM cart_items');
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error fetching cart items');
-    }
-  });
-  
-  app.get('/orders', async (req, res) => {
-    try {
-      const result = await db.query('SELECT * FROM orders');
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error fetching orders');
-    }
-  });
-  
-  app.get('/order_items', async (req, res) => {
-    try {
-      const result = await db.query('SELECT * FROM order_items');
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error fetching order items');
-    }
-  });
+app.use('/products', productsRouter);
+app.use('/orders', ordersRouter);
+app.use('/order_items', orderItemsRouter);
+app.use('/carts', cartsRouter);
+app.use('/cart_items', cartItemsRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
